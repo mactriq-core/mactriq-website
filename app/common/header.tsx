5b1cf064a30f17cd34ae3b53ceba9c8385aeff63
HarshitChejara <harshitchejara29@gmail.com>
2024-01-05 18:37:11 +0530

    changes

--- app/common/header.tsx
+++ app/common/header.tsx
@@ -86,9 +86,20 @@ const Header = () => {
         </Transition>
   
         <div className="hidden sm:block">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white mr-5"/>
+          <div className='flex items-center justify-center'>
+              <form action="" className="relative mx-auto flex">
+                  <input type="search" className="text-white text-xs peer cursor-pointer relative z-10 h-8 w-10 rounded-lg border bg-transparent  pr-6 outline-none focus:rounded-r-none focus:w-full focus:cursor-text focus:border-taupeGray focus:px-3"  placeholder="Type..."/>
+                  <button className="absolute top-0 right-0 bottom-0 my-auto h-8 w-10 px-3 bg-slate-100 rounded-lg peer-focus:relative peer-focus:rounded-l-none">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                      <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+                      </svg>
+                  </button>
+              </form>
+          &nbsp;
+          {/* <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white mr-5"/> */}
           <button onClick={openModal} type="button" className="text-[#15BFFD] rounded-full border border-[#15BFFD] py-2 px-5 leading-6 bg-[#2A3E84]"><Link href="">Contact Us</Link></button>
           <Modal isOpen={isModalOpen} onClose={closeModal} />
+          </div>
         </div>
       </Popover>
   );
